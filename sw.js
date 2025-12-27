fd1bd9778cb7581916ac5494c5ca5f590ed3acee
Enric-xX <almenarenric4@gmail.com>
2025-12-27 14:52:03 +0100

    Update service worker cache version to v2.6.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'coreai-v2.6.0'; // CAMBIA ESTO CUANDO SUBAS CAMBIOS
+const CACHE_NAME = 'coreai-v2.6.1';
 const assets = [
   './',
   './index.html',
@@ -8,34 +8,24 @@ const assets = [
   './manifest.json'
 ];
 
-// Instalar y guardar archivos en el PC
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Cacheando archivos maestros...');
-      return cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
-  self.skipWaiting(); 
+  self.skipWaiting();
 });
 
-// Activar y borrar versiones antiguas automáticamente
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
   );
   self.clients.claim();
 });
 
-// Servir archivos desde caché para velocidad máxima
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
